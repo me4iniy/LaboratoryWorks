3a5fc55d560dddad986d1cab9b4a6b5208b52e63
Kirill <me4iniy@gmail.com>
2022-11-05 17:21:51 +0500

    Добавьте файлы проекта.

---
+++ LaboratoryWorks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabWorks
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(23213);
+        }
+    }
+}
