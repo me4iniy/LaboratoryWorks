6ea7f66004f10cb226536e186cd4e8ca715edd50
Kirill <me4iniy@gmail.com>
2022-11-05 17:34:38 +0500

    Add description

---
+++ LaboratoryWorks/labWorkNumb1Task1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWorks
+{
+    public static class LabWorkNumb1Task1
+    {
+        /*
+        Пользователь вводит число. Если это целочисленное число (int.TryParse(...)), 
+            то вывести на экран символ соответствующий этому числу. 
+        Если это число с плавающей запятой, то сравнить это число с предыдущим введенным (float.NaN). 
+        Если эти числа равны, прекратить ввод, иначе повторить ввод. 
+        Также выполнение программы прекращается, если ввести символ q.
+         */
+
+
+    }
+}
